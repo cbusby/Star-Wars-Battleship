5ab7c7021f886b99f2c2c36e51f3c904e063a20d
David Nippa <dnippa@pillartechnology.com>
2018-11-01 13:50:10 -0400

    create React project

---
+++ src/components/Game/Game.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Game.css';
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World
+            </div>
+        );
+    }
+}
+
+export default Game;
