9b53a18a5938a3f8073b501eb10610af22e377b6
Michael Hall <mhall1473@gmail.com>
2018-11-02 13:18:23 -0400

    Converting content of index.tsx and Game.tsx to typescript

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import * as React from 'react';
+import logo from './aluminum-falcon.png'
 import './Game.css';
 
 class Game extends React.Component {
-    render() {
+    public render() {
         return (
-            <div>
-                <p>Hello Galaxy!</p>
-                <p>Remember, the Force will be with you, always.</p>
+            <div className="Game">
+                <header className="Game-header">
+                    <img src={logo} className="Game-logo" alt="logo" />
+                    <h1 className="Game-title">What a piece of junk!</h1>
+                </header>
             </div>
         );
     }
