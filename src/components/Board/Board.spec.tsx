097b97efb3ae2bf1c373d0454d8ca60ff2992687
Michael Hall <mhall1473@gmail.com>
2018-11-02 15:43:26 -0400

    Adding enzyme and config files

---
+++ src/components/Board/Board.spec.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+//import * as ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import Board from './Board';
+//import Square from '../Square/Square';
+
+it('renders without crashing', () => {
+    expect(shallow(<Board />).contains(<div className="Board" ></div>)).toBe(true);
+});
+
+// it('should render Squares', () => {
+//     let component = shallow(<Board />);
+//
+//     expect(component.find(Square)).toHaveLength(100);
+// });
