5a2c3e0a0cee0a18adf4e441ae0f97b83c5737bc
Michael Hall <mhall1473@gmail.com>
2019-01-24 16:23:41 -0500

    Committing at the end of the day so pair and continue to work

--- src/components/LandingPage/LandingPage.spec.tsx
+++ src/components/LandingPage/LandingPage.spec.tsx
@@ -1,15 +1,30 @@
-import { shallow } from 'enzyme';
+import {shallow} from 'enzyme';
 import * as React from 'react';
 import LandingPage from "./LandingPage";
 
 describe('LandingPage', () => {
     let component: import("../../../node_modules/@types/enzyme/index").ShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
+    let fakeCreateGameService: jest.Mock
+
+    const createGameResponse = {
+        "status": "AWAITING_SHIPS",
+        "player_1": {},
+        "player_2": {}
+    }
 
     beforeEach(() => {
-        component = shallow(<LandingPage />);
+        component = shallow(<LandingPage/>);
+        fakeCreateGameService = jest.fn().mockReturnValueOnce({ status: 201, createGameResponse});
     });
 
     it('renders a create game button', () => {
         expect(component.find('.CreateGameButton')).toHaveLength(1);
     });
+    it('renders a button with the text Start Game on it', () => {
+        expect(component.find('.CreateGameButton').first().text()).toBe("Start Game")
+    });
+    it("clicking button calls backend to create game", () => {
+        expect(fakeCreateGameService).toHaveBeenCalled()
+    });
+
 });
